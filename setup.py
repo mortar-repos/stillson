278bd532e5a706f6ee7af813ca4548f28d0e17a7
Chris Hoey <choey@mortardata.com>
2013-11-06 16:56:49 -0500

    adding tests

--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@ from setuptools import setup
 
 setup(
     name='stillson',
-    version='0.1.0',
+    version='0.1.1',
     author='Mortar Data',
     author_email='info@mortardata.com',
     packages=['stillson'],
-    scripts=['stillson/stillson.py'],
-    url='http://pypi.python.org/pypi/stillson/',
+    entry_points = {
+        'console_scripts': [
+            'stillson = stillson.stillson:main',
+        ]
+    },
+    url='https://github.com/mortardata/stillson/',
     license='LICENSE',
     description='Tool for generating config files using a template and environment variables.',
-    long_description=open('README.md').read(),
+    long_description='Tool for generating config files using a template and environment variables. https://github.com/mortardata/stillson',
     install_requires=[
         "mako >= 0.8.0",
     ],
--- stillson/stillson.py
+++ stillson/stillson.py
@@ -1,25 +1,48 @@
+#
+# Copyright 2013 Mortar Data Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 #!/usr/bin/python
 import os
 import sys
 from optparse import OptionParser
 from mako.template import Template
+from mako import exceptions
 
-def get_content(template_file):
-    template_file = open(template_file,mode='r')
-    template_lines = template_file.readlines()
-    template_content = ''
-    for line in template_lines:
-        template_content = template_content + line
-    return template_content
+class StillsonException(Exception):
+    pass
 
+class StillsonMissingEnvVariable(StillsonException):
+    pass
+
+def render(template_path,output_file):
+    template = Template(filename=template_path,strict_undefined = True)
+    try:
+        output_content = template.render(**os.environ)
+    except NameError as template_error:
+        raise StillsonMissingEnvVariable('The environment variable %s'%template_error)
+
+    output_file.write(output_content)
+    output_file.flush()
 
 def main():
-    parser = OptionParser(usage="usage: %prog template_file [options]",
+    parser = OptionParser(usage="usage: %prog template_path [options]",
                           version="%prog 1.0.0")
     parser.add_option('-o', '--output',
                       action='store',
                       dest='output',
-                      default='client.cfg',
                       help='[DEFAULT: %default] the path for the output file that gets created')
     (options,args ) = parser.parse_args()
 
@@ -28,18 +51,16 @@ def main():
         print error_msg
         sys.exit(1)
 
-    template_file = args[0]
-    try:
-       with open(template_file):
-           pass
-    except IOError:
-        print 'ERROR: The template file of %s does not exist'%template_file
-        sys.exit(1)
+    template_path = args[0]
+    if options.output:
+        output_file = open(options.output,'w')
+    else:
+        output_file = sys.stdout
 
-    template_content = get_content(template_file)
-    output_content = Template(template_content).render(**os.environ)
-    template_output = open(options.output,mode='w')
-    template_output.write(output_content)
+    render(template_path,output_file)
+
+    if options.output:
+        output_file.close()
 
 
 if __name__ == '__main__':
